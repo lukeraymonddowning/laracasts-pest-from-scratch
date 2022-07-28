ddd385ac338f6eb0aa00e03111aec0c5d432397e
Luke Downing <lukeraymonddowning@gmail.com>
2022-07-28 17:28:36 +0100

    Episode 6 (start)

--- tests/Feature/Images/ShowTest.php
+++ tests/Feature/Images/ShowTest.php
@@ -3,8 +3,7 @@
 use Illuminate\Support\Facades\Storage;
 
 it('can show supported image formats and options', function ($path, $options) {
-    Storage::fake();
-    Storage::put($path, file_get_contents(__DIR__ . "/../../fixtures/{$path}"));
+    Storage::fake()->put($path, file_get_contents(__DIR__ . "/../../fixtures/{$path}"));
 
     $response = $this->get(route('image', ['path' => $path, ...$options]));
     $response->assertOk();
